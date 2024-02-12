fefc61c4c99c990cbc40e2a6ea53390687ba9f88
junyub301 <junyub0301@gmail.com>
2024-02-12 15:25:33 +0900

    Feat: 해시태그 추가

--- src/components/posts/PostEditForm.tsx
+++ src/components/posts/PostEditForm.tsx
@@ -10,6 +10,8 @@ export default function PostEditForm() {
     const { id } = useParams();
     const [post, setPost] = useState<PostProps | null>(null);
     const [content, setContent] = useState<string>("");
+    const [hashTags, setHashTags] = useState<string[]>([]);
+    const [hashTag, setHashTag] = useState<string>("");
     const handleFileUpload = () => {};
     const navigate = useNavigate();
 
@@ -19,6 +21,7 @@ export default function PostEditForm() {
             const docSnap = await getDoc(docRef);
             setPost({ ...(docSnap.data() as PostProps), id: docSnap.id });
             setContent(docSnap?.data()?.content);
+            setHashTags(docSnap?.data()?.hashTags);
         }
     }, [id]);
 
@@ -38,12 +41,36 @@ export default function PostEditForm() {
         }
     };
 
+    const removeTag = (tag: string) => {
+        setHashTags(hashTags.filter((val) => val !== tag));
+    };
+    const onChangeHashtag = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const {
+            currentTarget: { value },
+        } = e;
+        setHashTag(value?.trim());
+    };
+    const handleKeyUp = (e: React.KeyboardEvent<HTMLInputElement>) => {
+        const {
+            code,
+            currentTarget: { value },
+        } = e;
+        if (code === "Space" && value?.trim() !== "") {
+            if (hashTags.includes(value?.trim())) {
+                toast.error("동일한 태그가 있습니다.");
+            } else {
+                setHashTags((pre) => [...(pre || []), value]);
+                setHashTag("");
+            }
+        }
+    };
+
     const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         try {
             if (post && id) {
                 const postRef = doc(db, "posts", id);
-                await updateDoc(postRef, { content });
+                await updateDoc(postRef, { content, hashTags });
                 navigate(`posts/${id}`);
                 toast.success("수정 완료");
             }
@@ -63,6 +90,29 @@ export default function PostEditForm() {
                 value={content}
                 onChange={onChange}
             />
+            <div className="post-form__hashtags">
+                <span className="post-form__hashtags-outputs">
+                    {hashTags.map((tag, index) => (
+                        <span
+                            className="post-form__hashtags-tag"
+                            key={index}
+                            onClick={() => removeTag(tag)}
+                        >
+                            #{tag}
+                        </span>
+                    ))}
+                </span>
+                <input
+                    type="text"
+                    className="post-form__input"
+                    name="hashtag"
+                    id="hashtag"
+                    placeholder="해시태그 + 스페이스바 입력"
+                    onChange={onChangeHashtag}
+                    onKeyUp={handleKeyUp}
+                    value={hashTag}
+                />
+            </div>
             <div className="post-form__submit-area">
                 <label htmlFor="file-input" className="post-form__file">
                     <FiImage className="post-form__file-icon" />
