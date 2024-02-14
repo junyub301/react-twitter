171d13c1ab21be367cc43bdad7fee3427d7516d5
junyub301 <junyub0301@gmail.com>
2024-02-14 22:12:12 +0900

    Feat: Comments 추가 /삭제

--- src/pages/posts/detail.tsx
+++ src/pages/posts/detail.tsx
@@ -1,21 +1,22 @@
+import CommentBox, { CommentProps } from "components/comments/CommentBox";
+import CommentForm from "components/comments/CommentForm";
 import Loader from "components/loader/Loader";
 import PostBox from "components/posts/PostBox";
-import { doc, getDoc } from "firebase/firestore";
+import PostHeader from "components/posts/PostHeader";
+import { doc, getDoc, onSnapshot } from "firebase/firestore";
 import { db } from "firebaseApp";
 import { PostProps } from "pages/home";
 import { useCallback, useEffect, useState } from "react";
-import { useNavigate, useParams } from "react-router-dom";
-import { IoIosArrowBack } from "react-icons/io";
-import PostHeader from "components/posts/PostHeader";
+import { useParams } from "react-router-dom";
 export default function PostDetail() {
     const { id } = useParams();
     const [post, setPost] = useState<PostProps | null>(null);
-    const navigate = useNavigate();
     const getPost = useCallback(async () => {
         if (id) {
             const docRef = doc(db, "posts", id);
-            const docSnap = await getDoc(docRef);
-            setPost({ ...(docSnap?.data() as PostProps), id: docSnap.id });
+            onSnapshot(docRef, (doc) => {
+                setPost({ ...(doc?.data() as PostProps), id: doc.id });
+            });
         }
     }, [id]);
 
@@ -28,7 +29,20 @@ export default function PostDetail() {
     return (
         <div className="post">
             <PostHeader />
-            {post ? <PostBox post={post} /> : <Loader />}
+            {post ? (
+                <>
+                    <PostBox post={post} />
+                    <CommentForm post={post} />
+                    {post.comments
+                        ?.slice(0)
+                        ?.reverse()
+                        ?.map((data: CommentProps, index: number) => (
+                            <CommentBox data={data} key={index} post={post} />
+                        ))}
+                </>
+            ) : (
+                <Loader />
+            )}
         </div>
     );
 }
