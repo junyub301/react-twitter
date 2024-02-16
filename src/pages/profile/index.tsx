12187a07a244a35d826b2e6c913e335155c4714a
junyub301 <junyub0301@gmail.com>
2024-02-16 22:49:37 +0900

    Feat: recoil set

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,10 +1,12 @@
 import { collection, onSnapshot, orderBy, query, where } from "@firebase/firestore";
+import { languagesState } from "atom";
 import PostBox from "components/posts/PostBox";
 import { AuthContext } from "context/AuthContext";
 import { db } from "firebaseApp";
 import { PostProps } from "pages/home";
 import { useContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router";
+import { useRecoilState } from "recoil";
 
 const DEFAULT_PROFILE = "/logo192.png";
 type TabType = "my" | "like";
@@ -15,6 +17,7 @@ export default function Profile() {
     const [likePosts, setLikePosts] = useState<PostProps[]>([]);
     const { user } = useContext(AuthContext);
     const navigate = useNavigate();
+    const [language, setLanguage] = useRecoilState(languagesState);
     useEffect(() => {
         if (user) {
             let postRef = collection(db, "posts");
@@ -40,6 +43,11 @@ export default function Profile() {
         }
     }, [user]);
 
+    const onClickLanguage = () => {
+        setLanguage(language === "ko" ? "en" : "ko");
+        localStorage.setItem("language", language === "ko" ? "en" : "ko");
+    };
+
     return (
         <div className="home">
             <div className="home__top">
@@ -52,13 +60,22 @@ export default function Profile() {
                         width={100}
                         height={100}
                     />
-                    <button
-                        className="profile__btn"
-                        type="button"
-                        onClick={() => navigate("/profile/edit")}
-                    >
-                        프로필 수정
-                    </button>
+                    <div className="profile__flex">
+                        <button
+                            className="profile__btn"
+                            type="button"
+                            onClick={() => navigate("/profile/edit")}
+                        >
+                            프로필 수정
+                        </button>
+                        <button
+                            className="profile__btn--language"
+                            type="button"
+                            onClick={onClickLanguage}
+                        >
+                            {language === "ko" ? "한국어" : "English"}
+                        </button>
+                    </div>
                 </div>
                 <div className="profile__text">
                     <div className="profile__name">{user?.displayName || "사용자님"}</div>
